94c53249c2f0d80a3980a18801d9a86850023569
leoenergy-dev <leoenergy2024@gmail.com>
2024-06-28 00:29:22 +0100

    teste

---
+++ src/components/index.d.ts
@@ -0,0 +1,21 @@
+import Script from "./Script.astro";
+import BanniereAccueil from "./Banniere_Accueil.astro";
+import BaseHead from "./BaseHead.astro";
+import BeforeFoot from "./BeforeFoot.astro";
+import Actu from "./Actu.astro";
+import Service from "./Service.astro";
+import Container from "./Container.astro";
+import Footer from "./Footer.astro";
+import Header from "./Header.astro";
+
+export {
+  Script,
+  BanniereAccueil,
+  BaseHead,
+  BeforeFoot,
+  Actu,
+  Service,
+  Container,
+  Footer,
+  Header,
+};
