0c9774278dcea9a2680ff848e262d3ec9ba4a0c1
leoenergy-dev <leoenergy2024@gmail.com>
2024-06-28 08:09:56 +0100

    description

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,5 @@
- export default {
-    title : 'Leo Energy',
-    description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum interdum eu eros malesuada pharetra. Maecenas vel lacinia ligula. Aliquam mi eros, lacinia vitae enim nec, posuere maximus magna. Curabitur fringilla massa sit amet nulla luctus, eu iaculis dolor consequat. '
- }
+export default {
+  title: "Leo Energy",
+  description:
+    "Leo Energy, fondée par Lionel MFEGUE le 4 juin 2024 à Vienne, est un Bureau d’études spécialisé dans les audits énergétiques et les conseils en efficacité énergétique. Nous sommes engagés à aider nos clients à optimiser leur consommation énergétique et à se conformer aux normes réglementaires en vigueur. Notre expertise couvre un large éventail de services, allant des audits réglementaires basés sur le Diagnostic de Performance Énergétique (DPE) pour les maisons individuelles et les bâtiments tertiaires, à l’accompagnement dans la mise en œuvre des recommandations issues de ces audits.",
+};
